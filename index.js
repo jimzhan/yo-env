845d6398dfb1b18622f2843fcb2b5b621b52ebb5
jimzhan <jim.zhan@me.com>
2018-07-24 10:53:34 +0800

    fix(ref): fix wrong cmd ref

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const path = require('path')
 const chalk = require('chalk')
 const shell = require('shelljs')
 
-const pkg = require(path.resolve(process.cwd(), 'package.json'))
 const env = process.env.NODE_ENV || 'development'
 const evt = process.env.npm_lifecycle_event
 const cmd = [evt, env].join(':')
+
+const pkg = require(path.resolve(process.cwd(), 'package.json'))
+const npm = process.platform === 'win32' ? 'npm.cmd' : 'npm'
 
 if (!(pkg.scripts && pkg.scripts[cmd])) {
   console.error(`${chalk.red('[var-env]')} "${cmd}" is missing`)
   process.exit(1)
 }
 
-shell.exec(`npm run ${pkg.scripts[cmd]}`, { silent: false })
+shell.exec(`{npm} run ${cmd}`, { silent: false })
+
